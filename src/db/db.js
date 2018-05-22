478c90606da0509acbfef9be244e28046450dae8
gerarddavila <gerarddavila@gmail.com>
2018-05-21 22:20:34 -0600

    Corrigiendo acceso a DB en AWS

--- src/db/db.js
+++ src/db/db.js
@@ -1,20 +1,11 @@
 const mysql = require('mysql');
 
 var connection = mysql.createConnection({
-  host: 'serverhost: db-apimultas.cadrtby0rver.us-west-2.rds.amazonaws.com',
-    user: 'adminapi',
-    password: 'Api2018Software',
-    database: 'apimultas',
+    host:'db-apimultas.cadrtby0rver.us-west-2.rds.amazonaws.com',
+    user:'adminapi',
+    password:'Api2018Software',
+    database:'apimultas',
     port: '3306'
-//  host: 'umgsm.com',
-  //user: 'apimultas_user',
-  //password: 'UserMultasDB2018',
-  //database: 'apimultas',
-  //port: '3306'
-  //host     : 'localhost',
-  //user     : 'root',
-  //password : '',
-  //database : 'db_api_multas'
 });
 connection.connect(function(err){
 if(!err) {
