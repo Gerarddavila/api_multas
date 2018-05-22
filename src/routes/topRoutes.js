2f8c4a303cc01811ac23daee4967093b394542e4
gerarddavila <gerarddavila@gmail.com>
2018-05-21 19:30:35 -0600

    Encabezado "datos" para topRoutes.js

--- src/routes/topRoutes.js
+++ src/routes/topRoutes.js
@@ -3,13 +3,13 @@ module.exports = function (app) {
 
   app.get('/top', (req, res) => {
       Top.getTop((err, data) => {
-      res.status(200).json({"Datos":data});
+      res.status(200).json({"datos":data});
     });
   });
 
   app.get('/top-costo', (req, res) => {
       Top.getTopCosto((err, data) => {
-      res.status(200).json(data);
+      res.status(200).json({"datos":data});
     });
   });
 }
